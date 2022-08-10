3bd8be6f9ae61bf09613ce4e97b6523c6b5beb4a
Las Safin <me@las.rs>
2022-08-10 14:50:18 +0000

    fix CI

--- flake.nix
+++ flake.nix
@@ -8,8 +8,19 @@
       supportedSystems = [ "x86_64-linux" "aarch64-linux" ];
       perSystem = nixpkgs.lib.genAttrs supportedSystems;
       nixpkgsFor = system: nixpkgs.legacyPackages.${system};
+      haskellPackagesFor = system: (nixpkgsFor system).haskell.packages.ghc923;
     in
     {
+      checks = perSystem (system: {
+        formatting = (nixpkgsFor system).runCommandNoCC "formatting" {} ''
+          cd ${self}
+          ./bin/format check
+          touch $out
+        '';
+      });
+      packages = perSystem (system: {
+        default = (haskellPackagesFor system).callPackage ./plutarch-core.nix {};
+      });
       devShells = perSystem (system: {
         default = import ./shell.nix { pkgs = nixpkgsFor system; };
       });
