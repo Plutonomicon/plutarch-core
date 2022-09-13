6c503566519c28e9f026b0413cd2a5748a2ef01c
Ari Fordsham <arifordsham@gmail.com>
2022-09-13 17:24:49 +0300

    Fix nix build

--- plutarch-core.nix
+++ plutarch-core.nix
@@ -1,8 +1,8 @@
-{ mkDerivation, base, generics-sop, lib }:
+{ mkDerivation, base, generics-sop, transformers, lib }:
 mkDerivation {
   pname = "plutarch-core";
   version = "0.1.0";
   src = ./.;
-  libraryHaskellDepends = [ base generics-sop ];
+  libraryHaskellDepends = [ base generics-sop transformers ];
   license = lib.licenses.mit;
 }
