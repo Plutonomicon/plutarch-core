e7696fb7120b5119b211935d9571ab9c62ef1630
Ari Fordsham <arifordsham@gmail.com>
2022-10-03 17:06:22 +0300

    ULC complete

--- Examples/ULC.hs
+++ Examples/ULC.hs
@@ -1,9 +1,12 @@
 {-# LANGUAGE UndecidableSuperClasses #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE UndecidableInstances #-}
+{-# OPTIONS_GHC -Wno-missing-export-lists #-}
+{-# OPTIONS_GHC -Wno-unused-imports #-}
 
 module Examples.ULC where
 
+import Plutarch.Core
 import Plutarch.Prelude
 import Plutarch.ULC ( ULC, compile )
 import Data.Functor.Identity ( Identity(runIdentity) )
@@ -14,26 +17,42 @@ pid = pcon $ PLam \x -> x
 -- | >>> runIdentity . compile $ pid
 -- Lam (Var 0)
 
-ptru :: (forall a b. PConstructable edsl (a #-> b)) => Term edsl (a #-> a #-> a)
+ptru ::
+  ( PConstructable edsl (PUnit #-> PUnit)
+  , PConstructable edsl (PUnit #-> PUnit #-> PUnit)
+  ) => Term edsl (PUnit #-> PUnit #-> PUnit)
 ptru = pcon $ PLam \t -> pcon $ PLam \_ -> t
 
 -- | >>> runIdentity . compile $ ptru
 -- Lam (Lam (Var 0))
 
-pfls :: (forall a b. PConstructable edsl (a #-> b)) => Term edsl (a #-> a #-> a)
+pfls ::
+  ( PConstructable edsl (PUnit #-> PUnit)
+  , PConstructable edsl (PUnit #-> PUnit #-> PUnit)
+  ) => Term edsl (PUnit #-> PUnit #-> PUnit)
 pfls = pcon $ PLam \_ -> pcon $ PLam \f -> f
 
 -- | >>> runIdentity . compile $ pfls
 -- Lam (Lam (Var 1))
 
 pif ::
-  ( forall a b. PConstructable edsl (a #-> b)
-  , IsPType edsl a
-  ) => Term edsl ((a #-> a #-> a) #-> a #-> a #-> a)
+  forall edsl.
+  ( PLC edsl
+  , PConstructable edsl (PUnit #-> PUnit)
+  , PConstructable edsl (PUnit #-> PUnit #-> PUnit)
+  , PConstructable edsl ((PUnit #-> PUnit #-> PUnit) #-> PUnit #-> PUnit #-> PUnit)
+  , IsPType edsl PUnit
+  ) => Term edsl ((PUnit #-> PUnit #-> PUnit) #-> PUnit #-> PUnit #-> PUnit)
 pif = pcon $ PLam \l -> pcon $ PLam \m -> pcon $ PLam \n -> l # m # n
 
-pnot :: (forall a b. PConstructable edsl (a #-> b)) => Term edsl (((a #-> a #-> a) #-> (a #-> a #-> a) #-> (a #-> a #-> a)) #-> (a #-> a #-> a))
-pnot = pcon $ PLam \x -> x # pfls # ptru
+pnot ::
+  ( PLC edsl
+  , PConstructable edsl (PUnit #-> PUnit)
+  , PConstructable edsl (PUnit #-> PUnit #-> PUnit)
+  , PConstructable edsl ((PUnit #-> PUnit #-> PUnit) #-> PUnit #-> PUnit #-> PUnit)
+  , IsPType edsl PUnit
+  ) => Term edsl ((PUnit #-> PUnit #-> PUnit) #-> PUnit #-> PUnit #-> PUnit)
+pnot = plam \b x y -> b # y # x
 
-x :: ULC
-x = runIdentity . compile $ pnot # ptru
+-- | >>> runIdentity . compile $ pnot # ptru
+-- App (Lam (Lam (Lam (App (App (Var 0) (Var 2)) (Var 1))))) (Lam (Lam (Var 0)))
