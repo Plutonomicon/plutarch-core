ec829ae64aebed9c1710b4352fea3bdbc509a32a
Las Safin <me@las.rs>
2023-02-26 23:47:36 +0000

    Try simplifying `Term`, redoing it from scratch.

--- Plutarch2New2.hs
+++ Plutarch2New2.hs
@@ -2,6 +2,19 @@
 {-# LANGUAGE UndecidableInstances #-}
 {-# LANGUAGE LinearTypes #-}
 {-# LANGUAGE NoFieldSelectors #-}
+{-# OPTIONS_GHC -Wno-unused-type-patterns #-}
+{-# OPTIONS_GHC -Wno-unused-packages -Wno-unused-imports -Wno-missing-export-lists #-}
+
+-- TODO:
+-- - Use stable names to reduce work
+-- - Hashing partial interpreter for hoisting
+-- - Example ULC interpreter
+-- - Ergonomic op-level definitions
+-- - Optimisations
+-- - Data types
+-- - PAsData
+-- - Optics
+-- - Plugins for syntax
 
 module Plutarch2New where
 
@@ -12,17 +25,21 @@ import Numeric.Natural (Natural)
 import Data.Functor.Const (Const (Const), getConst)
 import GHC.TypeLits
 import GHC.Exts
+import Unsafe.Coerce
+import Data.Type.Equality
 
 witness :: c => Proxy c -> ()
 witness c = let _ = witness c in ()
 
-data ElemOf a as = UnsafeElemOf Natural (TypeRep a)
+data ElemOf a as where
+  Here :: ElemOf a (a ': as)
+  There :: ElemOf a as -> ElemOf a (a' ': as)
 
-here :: Typeable a => ElemOf a (a ': as)
-here = UnsafeElemOf 0 typeRep
+here :: ElemOf a (a ': as)
+here = Here
 
 there :: ElemOf a as -> ElemOf a (a' ': as)
-there (UnsafeElemOf i t) = UnsafeElemOf (i + 1) t
+there = There
 
 class CElemOf a as where
   elemOf :: ElemOf a as
@@ -66,7 +83,39 @@ type SwapInList2 :: [[a]] -> [[a]] -> a -> a -> Type
 data SwapInList2 xss yss x y where
   SwapInList2 :: SwapInList xss yss xs ys -> SwapInList xs ys x y -> SwapInList2 xss yss x y
 
---data Swappable (l :: Language) (l' :: Language) where
+-- ListEqMod1 xs ys x tells us that ys is equal to xs but has an extra x somewhere
+type ListEqMod1 :: [a] -> [a] -> a -> Type
+data ListEqMod1 xs ys x where
+  ListEqMod1Base :: ListEqMod1 xs (x ': xs) x
+  ListEqMod1Step :: ListEqMod1 xs ys x -> ListEqMod1 (y ': xs) (y ': ys) x
+
+class CListEqMod1 xs ys x where
+  listEqMod1 :: ListEqMod1 xs ys x
+instance CListEqMod1 '[x] '[x, x] x where
+  listEqMod1 = ListEqMod1Base
+instance {-# OVERLAPPABLE #-} CListEqMod1 xs (x ': xs) x where
+  listEqMod1 = ListEqMod1Base
+instance {-# OVERLAPPABLE #-} CListEqMod1 xs ys x => CListEqMod1 (y ': xs) (y ': ys) x where
+  listEqMod1 = ListEqMod1Step listEqMod1
+
+type RemoveFrom :: [a] -> a -> [a]
+type family RemoveFrom xs x where
+  RemoveFrom (x ': xs) x = xs
+  RemoveFrom (y ': xs) x = y ': RemoveFrom xs x
+
+-- Permute1 xs ys means that xs ys are equal one element being moved.
+type Permute1 :: [a] -> [a] -> Type
+data Permute1 xs ys where
+  Permute1Base :: ListEqMod1 xs ys x -> Permute1 (x ': xs) ys
+  Permute1BaseInv :: ListEqMod1 xs ys x -> Permute1 ys (x ': xs)
+  Permute1Step :: Permute1 xs ys -> Permute1 (x ': xs) (x ': ys)
+
+class CPermute1 xs ys where
+  permute1 :: Permute1 xs ys
+instance CListEqMod1 xs ys x => CPermute1 (x ': xs) ys where
+  permute1 = Permute1Base listEqMod1
+instance CPermute1 xs ys => CPermute1 (x ': xs) (x ': ys) where
+  permute1 = Permute1Step permute1
 
 class
   ( KnownBool (IsLanguageContractible l)
@@ -79,7 +128,7 @@ class
     forall f lss lss' l0 l1 tag.
     (Functor f, IsLanguage l0, IsLanguage l1) =>
     SwapInList2 lss lss' l0 l1 ->
-    Interpreter l0 l1 f tag ->
+    Interpreter l0 l1 f ->
     L l Term lss tag ->
     f (L l Term lss' tag)
   interpretIn = undefined
@@ -92,6 +141,7 @@ type family ListAppend xs ys where
 type ListAppendAll :: [[a]] -> [a]
 type family ListAppendAll xss where
   ListAppendAll '[] = '[]
+  ListAppendAll '[xs] = xs
   ListAppendAll (xs ': xss) = ListAppend xs (ListAppendAll xss)
 
 type LengthOf :: [a] -> Type
@@ -125,32 +175,43 @@ data Term ls tag where
   Send :: IsLanguage l => LengthsOf lss -> TypeRep l -> L l Term lss tag -> Term (l ': ListAppendAll lss) tag
   Expand :: IsLanguageExpansible l ~ 'True => Term ls tag -> Term (l ': ls) tag
   Contract :: IsLanguageContractible l ~ 'True => ElemOf l ls -> Term (l ': ls) tag -> Term ls tag
+  SwapAt :: Permute1 ls ls' -> Term ls tag -> Term ls' tag
   Swap :: Term (l ': l' ': ls) tag -> Term (l' ': l ': ls) tag
   Pack :: Term (l ': l' ': ls) tag -> Term (Union l l' ': ls) tag
   Unpack :: Term (Union l l' ': ls) tag -> Term (l ': l' ': ls) tag
   Rotate :: Term (l0 ': l1 ': l2 ': ls) tag -> Term (l2 ': l0 ': l1 ': ls) tag
 
-data Interpreter l l' f tag
+data Interpreter l l' f
   = Interpreter
-    (forall lss. L l (TermF f) lss tag -> f (L l' Term lss tag))
-    (forall lss. L l Term lss tag -> f (L l' Term lss tag))
+    (forall lss tag. L l (TermF f) lss tag -> f (L l' Term lss tag))
+    (forall lss tag. L l Term lss tag -> f (L l' Term lss tag))
+    --(forall lss tag. L l ())
 
-runInterpreterStep :: Interpreter l l' f tag -> L l (TermF f) lss tag -> f (L l' Term lss tag)
+runInterpreterStep :: Interpreter l l' f -> L l (TermF f) lss tag -> f (L l' Term lss tag)
 runInterpreterStep (Interpreter step _) = step
 
-runInterpreterBase :: Interpreter l l' f tag -> L l Term lss tag -> f (L l' Term lss tag)
+runInterpreterBase :: Interpreter l l' f -> L l Term lss tag -> f (L l' Term lss tag)
 runInterpreterBase (Interpreter _ base) = base
 
 interpret ::
   forall f ls ls' l l' tag.
   (Functor f, IsLanguage l, IsLanguage l') =>
   SwapInList ls ls' l l' ->
-  Interpreter l l' f tag ->
+  Interpreter l l' f ->
   Term ls tag ->
   f (Term ls' tag)
-interpret SWBase intr (Send lengths tyrep x) = Send undefined undefined <$> runInterpreterBase intr x
+interpret SWBase intr (Send lengths _ x) = Send lengths typeRep <$> runInterpreterBase intr x
 
-sendLin :: (IsLanguage l, CLengthsOf lss, Typeable l) => L l Term lss tag -> Term (l ': ListAppendAll lss) tag
+contract :: (IsLanguageContractible l ~ 'True, CElemOf l ls) => Term (l ': ls) tag -> Term ls tag
+contract x = Contract elemOf x
+
+swapAt :: CPermute1 ls ls' => Term ls tag -> Term ls' tag
+swapAt x = SwapAt permute1 x
+
+bring :: forall l ls' ls tag. (ls' ~ RemoveFrom ls l, CListEqMod1 ls' ls l) => Term ls tag -> Term (l ': ls') tag
+bring x = SwapAt (Permute1BaseInv listEqMod1) x
+
+sendLin :: (IsLanguage l, CLengthsOf lss) => L l Term lss tag -> Term (l ': ListAppendAll lss) tag
 sendLin x = Send lengthsOf typeRep x
 
 --class AllEqual (lss :: [[Language]]) (ls :: [Language])
@@ -172,23 +233,42 @@ data Eff (a :: PType)
 data TypeInfo (a :: PType)
 
 data Arithmetic
-data instance L Arithmetic _term _ls _tag where
+data instance L Arithmetic term ls tag where
   Add :: term ls0 (Expr w Int) -> term ls1 (Expr w Int) -> L Arithmetic term '[ls0, ls1] (Expr w Int)
+  Mult :: term ls0 (Expr w Int) -> term ls1 (Expr w Int) -> L Arithmetic term '[ls0, ls1] (Expr w Int)
   IntLiteral :: Int -> L Arithmetic term '[] (Expr w Int)
   Double :: term ls0 (Expr w Int) -> L Arithmetic term '[ls0] (Expr w Int)
   IntTy :: L Arithmetic term '[] (TypeInfo Int)
+  IsZero :: term ls0 (Expr w Int) -> L Arithmetic term '[ls0] (Expr w Bool)
 instance IsLanguage Arithmetic where
   type IsLanguageContractible _ = 'True
   type IsLanguageExpansible _ = 'True
   interpretIn (SwapInList2 SWBase idx) f (Add x y) = flip Add y <$> interpret idx f x
   interpretIn (SwapInList2 (SWInc SWBase) idx) f (Add x y) = Add x <$> interpret idx f y
   interpretIn (SwapInList2 (SWInc (SWInc idx')) _) _ (Add _ _) = case idx' of
+  interpretIn (SwapInList2 SWBase idx) f (Mult x y) = flip Mult y <$> interpret idx f x
+  interpretIn (SwapInList2 (SWInc SWBase) idx) f (Mult x y) = Mult x <$> interpret idx f y
+  interpretIn (SwapInList2 (SWInc (SWInc idx')) _) _ (Mult _ _) = case idx' of
   interpretIn (SwapInList2 SWBase idx) f (Double x) = Double <$> interpret idx f x
+  interpretIn (SwapInList2 SWBase idx) f (IsZero x) = IsZero <$> interpret idx f x
 
-data HasLinearVar (a :: PType)
-data instance L (HasLinearVar _a) _term _ls _tag where
-  LinearVar :: L (HasLinearVar a) term '[] (LinExpr a)
-instance Typeable a => IsLanguage (HasLinearVar a) where
+data Arithmetic2
+data instance L Arithmetic2 term ls tag where
+  Arithmetic2 :: term (Arithmetic ': ls0) tag -> L Arithmetic2 term '[ls0] tag
+
+data Conditionals
+data instance L Conditionals term ls tag where
+  If :: term ls0 (Expr w Bool) -> term ls1 (Expr w a) -> term ls1 (Expr w a) -> L Conditionals term '[ls0, ls1] (Expr w a)
+instance IsLanguage Conditionals where
+  type IsLanguageContractible _ = 'True
+  type IsLanguageExpansible _ = 'True
+  interpretIn (SwapInList2 SWBase idx) f (If b x y) = flip (flip If x) y <$> interpret idx f b
+  interpretIn (SwapInList2 (SWInc SWBase) idx) f (If b x y) = undefined (interpret idx f x) (interpret idx f y)
+
+data HasLinVar (a :: PType)
+data instance L (HasLinVar a) term ls tag where
+  LinVar :: L (HasLinVar a) term '[] (LinExpr a)
+instance Typeable a => IsLanguage (HasLinVar a) where
   type IsLanguageContractible _ = 'False
   type IsLanguageExpansible _ = 'False
 
@@ -196,41 +276,49 @@ data (a :: PType) --> (b :: PType)
 infixr 0 -->
 
 data LinearLC
-data instance L LinearLC _term _ls _tag where
-  LinLam :: term (HasLinearVar a ': ls0) (LinExpr b) -> L LinearLC term '[ls0] (LinExpr (a --> b))
-  LinApp :: term ls0 (LinExpr (a --> b)) -> term ls1 (LinExpr a) -> L LinearLC term '[ls0, ls1] (LinExpr b)
+data instance L LinearLC term ls tag where
+  LinLam :: term (HasLinVar a ': ls0) (LinExpr b) -> L LinearLC term '[ls0] (LinExpr (a --> b))
+  LinLam' :: term (HasLinVar a ': LinearLC ': ls0) (LinExpr b) -> L LinearLC term '[ls0] (LinExpr (a --> b))
+  LinApp :: term ls0 (TypeInfo a) -> term ls1 (LinExpr (a --> b)) -> term ls2 (LinExpr a) -> L LinearLC term '[ls0, ls1, ls2] (LinExpr b)
 instance IsLanguage LinearLC where
   type IsLanguageContractible _ = 'True
   type IsLanguageExpansible _ = 'True
 
 data HasFreeVar (a :: PType)
-data instance L (HasFreeVar _a) _term _ls _tag where
+data instance L (HasFreeVar _a) term ls tag where
   FreeVar :: L (HasFreeVar a) term '[] (FreeExpr a)
 instance Typeable a => IsLanguage (HasFreeVar a) where
   type IsLanguageContractible _ = 'True
   type IsLanguageExpansible _ = 'True
 
 data FreeLC
-data instance L FreeLC _term _ls _tag where
+data instance L FreeLC term ls tag where
   MakeLin :: term ls0 (FreeExpr a) -> L FreeLC term '[ls0] (LinExpr a)
   FreeLam :: term (HasFreeVar a ': ls0) (FreeExpr a) -> L FreeLC term '[ls0] (FreeExpr (a -> b))
-  FreeApp :: term ls0 (FreeExpr (a -> b)) -> term ls1 (FreeExpr a) -> L FreeLC term '[ls0, ls1] (FreeExpr b)
+  FreeApp :: term ls0 (TypeInfo a) -> term ls1 (FreeExpr (a -> b)) -> term ls2 (FreeExpr a) -> L FreeLC term '[ls0, ls1, ls2] (FreeExpr b)
 instance IsLanguage FreeLC where
   type IsLanguageContractible _ = 'True
   type IsLanguageExpansible _ = 'True
 
 data HOAS_LC
-data instance L HOAS_LC _term _ls _tag where
+data instance L HOAS_LC term ls tag where
   HOAS_Lam :: (term '[] (FreeExpr a) -> term ls0 (FreeExpr b)) -> L HOAS_LC term '[ls0] (FreeExpr (a -> b))
 instance IsLanguage HOAS_LC where
   type IsLanguageContractible _ = 'True
   type IsLanguageExpansible _ = 'True
 
-double :: Term '[LinearLC] (LinExpr (Int --> Int))
-double = sendLin $ LinLam $ sendLin LinearVar
+data HasExp
+data instance L HasExp term ls tag where
+  Exp :: term ls0 (Expr w Int) -> term ls1 (Expr w Int) -> L HasExp term '[ls0, ls1] (Expr w Int)
 
-add :: Term '[LinearLC, Arithmetic, LinearLC] (LinExpr (Int --> Int --> Int))
-add = Unpack $ Swap $ sendLin $ LinLam $ Swap $ Pack $ sendLin $ LinLam $ Swap $ sendLin $ Add (sendLin (LinearVar @Int)) (sendLin (LinearVar @Int))
+double :: Term '[LinearLC] (LinExpr (Int --> Int))
+double = sendLin $ LinLam $ sendLin LinVar
+
+add :: Term '[LinearLC, Arithmetic] (LinExpr (Int --> Int --> Int))
+add = contract $ sendLin $ LinLam $ bring $ sendLin $ LinLam $ bring $ sendLin $ Add (sendLin (LinVar @Int)) (sendLin (LinVar @Int))
+
+add_expanded :: Term '[LinearLC, Arithmetic] (LinExpr (Int --> Int --> Int))
+add_expanded = contract $ contract $ contract $ sendLin $ LinLam $ bring $ sendLin $ LinApp (sendLin IntTy) add (sendLin LinVar)
 
 addSelf :: Term '[FreeLC, Arithmetic] (FreeExpr (Int -> Int))
 addSelf = sendLin $ FreeLam $ Swap $ Contract elemOf $ Rotate $ sendLin $ Add (sendLin FreeVar) (sendLin $ FreeVar)
@@ -241,27 +329,63 @@ addSelf2 = sendLin $ HOAS_Lam \x -> sendLin $ Add x x
 eleven :: Term '[Arithmetic] (FreeExpr Int)
 eleven = Contract here $ Contract here $ sendLin $ Add (sendLin $ IntLiteral 5) (sendLin $ IntLiteral 6)
 
+linLam ::
+  (CListEqMod1 ls' ls (HasLinVar a), CLengthOf ls', CElemOf LinearLC ls') =>
+  Term ls (LinExpr b) -> Term ls' (LinExpr (a --> b))
+linLam x = contract $ sendLin $ LinLam $ SwapAt (Permute1BaseInv listEqMod1) x
+
+ifZeroThenPlusSixElseId :: Term '[LinearLC, Conditionals, Arithmetic] (LinExpr (Int --> Int --> Int))
+ifZeroThenPlusSixElseId = contract $ bring @Arithmetic $ contract $ sendLin $ LinLam @Term @Int $ bring $ sendLin $ LinLam @Term @Int $ bring $ sendLin $ If (sendLin $ IsZero (sendLin (LinVar @Int))) (contract $ sendLin $ Add (sendLin $ IntLiteral 6) (sendLin LinVar)) (Expand $ sendLin (LinVar @Int))
+
+
 data DummyLang
-data instance L DummyLang _term _ls _tag where
+data instance L DummyLang term ls tag where
 instance IsLanguage DummyLang where
   type IsLanguageExpansible _ = 'True
   type IsLanguageContractible _ = 'True
 
+type InterpArithmetic :: TermTy
+data family InterpArithmetic ls tag
+newtype instance InterpArithmetic ls (Expr w a) = IA a
+
+data family ExtractType :: Tag -> Type
+newtype instance ExtractType (Expr w a) = ExtractType a
+data instance ExtractType (TypeInfo a) = ExtractTypeInfo
+
 interpretArithmetic :: Term '[Arithmetic] (FreeExpr Int) -> Int
 interpretArithmetic x = getConst $ interpret SWBase (Interpreter step base) x where
-  step :: L Arithmetic (TermF (Const Int)) ls' (FreeExpr Int) -> Const Int (L DummyLang Term ls' (FreeExpr Int))
+  step :: L Arithmetic (TermF (Const Int)) lss tag -> Const Int (L DummyLang Term lss tag)
   step (IntLiteral n) = Const n
   step (Add (TermF (Const x)) (TermF (Const y))) = Const (x + y)
+  step (Mult (TermF (Const x)) (TermF (Const y))) = Const (x * y)
   step (Double (TermF (Const x))) = Const $ x + x
-  base :: L Arithmetic Term ls' (FreeExpr Int) -> Const Int (L DummyLang Term ls' (FreeExpr Int))
+  step (IsZero (TermF (Const _))) = error "FIXME" -- Const $ x == 0
+  step IntTy = error "FIXME: make impossible"
+  base :: L Arithmetic Term ls' tag -> Const Int (L DummyLang Term ls' tag)
   base (IntLiteral n) = Const n
-  base (Add _ _) = error "FIXME: prove impossible"
-  base (Double _) = error "FIXME: prove impossible"
+  base _ = error "FIXME: prove impossible"
+  f :: L Arithmetic InterpArithmetic lss tag -> ExtractType tag
+  f (IntLiteral n) = ExtractType n
+  f (Add (IA x) (IA y)) = ExtractType $ x + y
+  f (Mult (IA x) (IA y)) = ExtractType $ x + y
+  f (Double (IA x)) = ExtractType $ x + x
+  f (IsZero (IA x)) = ExtractType $ x == 0
+  f IntTy = ExtractTypeInfo
+  g :: L Conditionals InterpArithmetic lss tag -> ExtractType tag
+  g (If (IA b) (IA x) (IA y)) = ExtractType $ if b then x else y
+  eta_reduce :: L LinearLC Term lss tag -> L LinearLC Term lss tag
+  --eta_reduce (LinApp (X False x) y) = X False $ LinApp x y
+  --eta_reduce (LinApp (X True x) y) = X False $ LinApp x y
+  eta_reduce (LinLam body) = undefined
+
+eta_reduce_opt :: Term '[LinearLC, Arithmetic] tag -> Term '[LinearLC, Arithmetic] tag
+eta_reduce_opt (Contract Here (Send _ _ (LinLam (Send _ _ (LinApp _ lhs rhs))))) = _
+eta_reduce_opt _ = error "unimplemented"
 {-
 
 jkk
 data OnlyAdds
-data instance L OnlyAdds _term _ls _tag where
+data instance L OnlyAdds term ls tag where
   OnlyAdd :: term ls (Expr Int) -> term ls (Expr Int) -> L OnlyAdds term ls (Expr Int)
 instance IsLanguage OnlyAdds where
   type IsLanguageContractible _ = 'True
---
+++ Plutarch2New3.hs
@@ -0,0 +1,250 @@
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE LinearTypes #-}
+{-# LANGUAGE NoFieldSelectors #-}
+{-# LANGUAGE PartialTypeSignatures #-}
+{-# OPTIONS_GHC -Wno-unused-type-patterns #-}
+{-# OPTIONS_GHC -Wno-unused-packages -Wno-unused-imports -Wno-missing-export-lists #-}
+
+-- TODO:
+-- - Use stable names to reduce work
+-- - Hashing partial interpreter for hoisting
+-- - Example ULC interpreter
+-- - Ergonomic op-level definitions
+-- - Optimisations
+-- - Data types
+-- - PAsData
+-- - Optics
+-- - Plugins for syntax
+
+module Plutarch2New3 where
+
+import Data.Kind (Type)
+import Type.Reflection (Typeable, TypeRep, typeRep)
+import Data.Proxy (Proxy)
+import Numeric.Natural (Natural)
+import Data.Functor.Const (Const (Const), getConst)
+import GHC.TypeLits
+import GHC.Exts
+import Unsafe.Coerce
+import Data.Type.Equality
+
+type Tag = Type
+type TermTy = [Language] -> Tag -> Type
+type Language = Type
+data family L (l :: Language) :: TermTy -> [[Language]] -> Tag -> Type
+
+type ListAppend :: [a] -> [a] -> [a]
+type family ListAppend xs ys where
+  ListAppend '[] ys = ys
+  ListAppend (x ': xs) ys = x ': (ListAppend xs ys)
+
+type ListAppendAll :: [[a]] -> [a]
+type family ListAppendAll xss where
+  ListAppendAll '[] = '[]
+  ListAppendAll '[xs] = xs
+  ListAppendAll (xs ': xss) = ListAppend xs (ListAppendAll xss)
+
+type LengthOf :: [a] -> Type
+data LengthOf xs where
+  LengthZero :: LengthOf '[]
+  LengthSucc :: LengthOf xs -> LengthOf (x ': xs)
+
+class CLengthOf (xs :: [a]) where
+  lengthOf :: LengthOf xs
+instance CLengthOf '[] where
+  lengthOf = LengthZero
+instance CLengthOf xs => CLengthOf (x ': xs) where
+  lengthOf = LengthSucc lengthOf
+
+type LengthsOf :: [[a]] -> Type
+data LengthsOf xss where
+  LNil :: LengthsOf '[]
+  LCons :: LengthOf xs -> LengthsOf xss -> LengthsOf (xs ': xss)
+
+class CLengthsOf xss where
+  lengthsOf :: LengthsOf xss
+instance CLengthsOf '[] where
+  lengthsOf = LNil
+instance (CLengthOf xs, CLengthsOf xss) => CLengthsOf (xs ': xss) where
+  lengthsOf = LCons lengthOf lengthsOf
+
+-- ListEqMod1 xs ys x tells us that ys is equal to xs but has an extra x somewhere
+type ListEqMod1 :: [a] -> [a] -> a -> Type
+data ListEqMod1 xs ys x where
+  ListEqMod1Base :: ListEqMod1 xs (x ': xs) x
+  ListEqMod1Step :: ListEqMod1 xs ys x -> ListEqMod1 (y ': xs) (y ': ys) x
+
+class CListEqMod1 xs ys x where
+  listEqMod1 :: ListEqMod1 xs ys x
+instance CListEqMod1 xs (x ': xs) x where listEqMod1 = ListEqMod1Base
+instance CListEqMod1 xs ys x => CListEqMod1 (y ': xs) (y ': ys) x where listEqMod1 = ListEqMod1Step listEqMod1
+
+-- `Flattened xss xs` proves that `xs ~ ListAppendAll xss`
+type Flattened :: [[a]] -> [a] -> Type
+data Flattened xss xs where
+  FlattenedBase :: Flattened '[] '[]
+  FlattenedBase' :: Flattened '[xs] xs
+  FlattenedSeg :: Flattened xss xs -> Flattened ('[x] ': xss) (x ': xs)
+  FlattenedStep :: Flattened (xs' ': xss) xs -> Flattened ((x ': xs') ': xss) (x ': xs)
+
+class CFlattened xss xs | xss -> xs where
+  flattened :: Flattened xss xs
+instance CFlattened '[] '[] where flattened = FlattenedBase
+instance CFlattened xss xs => CFlattened ('[x] ': xss) (x ': xs) where flattened = FlattenedSeg flattened
+instance CFlattened ((x' ': xs') ': xss) xs => CFlattened ((x ': x' ': xs') ': xss) (x ': xs) where flattened = FlattenedStep flattened
+
+type SwapInList :: [a] -> [a] -> a -> a -> Type
+data SwapInList xs ys x y where
+  SWBase :: SwapInList (x ': xs) (y ': xs) x y
+  SWInc :: SwapInList xs ys x y -> SwapInList (z ': xs) (z ': ys) x y
+
+class CSwapInList xs ys x y where
+  swapInList :: SwapInList xs ys x y
+instance CSwapInList (x ': xs) (y ': xs) x y where swapInList = SWBase
+instance CSwapInList xs ys x y => CSwapInList (z ': xs) (z ': ys) x y where swapInList = SWInc swapInList
+
+-- RB xss xs tells us where each element in xs comes from in xss
+-- TODO: Establish a canonical representation that either puts
+-- all RB1s first, or prevent using RB2 when RB1 could be used.
+type RB :: [[a]] -> [a] -> Type
+data RB xss xs where
+  RB0 :: RB '[] '[]
+  RB1 :: RB xss xs -> RB ('[] ': xss) xs
+  RB2 :: ListEqMod1 ys ys' x -> SwapInList xss xss' ys ys' -> RB xss xs -> RB xss' (x ': xs)
+
+type RemoveFrom :: [a] -> a -> [a]
+type family RemoveFrom xs x where
+  RemoveFrom (x ': xs) x = xs
+  RemoveFrom (y ': xs) x = y ': RemoveFrom xs x
+
+type family ListAppendReverse xs ys where
+  ListAppendReverse '[] ys = ys
+  ListAppendReverse (x ': xs) ys = ListAppendReverse xs (x ': ys)
+
+type family RemoveFrom2Helper rest xs xss x where
+  RemoveFrom2Helper rest '[] xss x = ListAppendReverse rest '[] ': RemoveFrom2 xss x
+  RemoveFrom2Helper rest (x ': xs) xss x = ListAppendReverse rest xs ': xss
+  RemoveFrom2Helper rest (y ': xs) xss x = RemoveFrom2Helper (y ': rest) xs xss x
+
+type family RemoveFrom2 xss x where
+  RemoveFrom2 ('[] ': xss) x = RemoveFrom2 xss x
+  RemoveFrom2 ((x ': xs) ': xss) x = xs ': xss
+  RemoveFrom2 (xs ': xss) x = RemoveFrom2Helper '[] xs xss x
+
+type FindWithHelper :: [a] -> [[a]] -> a -> [a]
+type family FindWithHelper xs xss x where
+  FindWithHelper '[] xss x = FindWith xss x
+  FindWithHelper (x ': xs) xss x = x ': xs
+  FindWithHelper (y ': xs) xss x = y ': FindWithHelper xs xss x
+
+type FindWith :: [[a]] -> a -> [a]
+type family FindWith xss x where
+  FindWith (xs ': xss) x = FindWithHelper xs xss x
+
+data ListOf xs x where
+  ListOfNil :: ListOf '[] x
+  ListOfCons :: ListOf xs x -> ListOf (x ': xs) x
+
+class CListOf xs x where
+  listOf :: ListOf xs x
+instance CListOf '[] x where listOf = ListOfNil
+instance CListOf xs x => CListOf (x ': xs) x where listOf = ListOfCons listOf
+
+class CRBInto xss xs where
+  rbinto :: RB xss xs
+instance CListOf xss '[] => CRBInto xss '[] where
+  rbinto = f listOf where
+    f :: forall xss. ListOf xss '[] -> RB xss '[]
+    f ListOfNil = RB0
+    f (ListOfCons x) = RB1 (f x)
+instance
+  ( ys ~ RemoveFrom ys' x
+  , ys' ~ FindWith xss' x
+  , xss ~ RemoveFrom2 xss' x
+  , CListEqMod1 ys ys' x
+  , CSwapInList xss xss' ys ys'
+  , CRBInto xss xs
+  ) => CRBInto xss' (x ': xs) where
+  rbinto = RB2 (listEqMod1 :: ListEqMod1 ys ys' x) (swapInList :: SwapInList xss xss' ys ys') rbinto
+
+class CRBFlat xss xs | xss -> xs where
+  rbflat :: RB xss xs
+instance CRBFlat ('[] :: [[a]]) ('[] :: [a]) where rbflat = RB0
+instance CRBFlat xss xs => CRBFlat ('[x] ': xss) (x ': xs) where
+  rbflat = RB2 ListEqMod1Base SWBase (RB1 (rbflat :: RB xss xs))
+instance CRBFlat ((x' ': xs') ': xss) xs => CRBFlat ((x ': x' ': xs') ': xss) (x ': xs) where
+  rbflat = RB2 ListEqMod1Base SWBase (rbflat :: RB ((x' ': xs') ': xss) xs)
+
+type Term :: TermTy
+data Term ls tag where
+  Send :: RB lss ls' -> ListEqMod1 ls' ls l -> L l Term lss tag -> Term ls tag
+  --Bring :: ListEqMod1 ls' ls l -> Term ls tag -> Term (l ': ls') tag
+
+data Expr (a :: Type)
+
+data Bools
+data instance L Bools term ls tag where
+  Xor :: term ls0 (Expr Bool) -> term ls1 (Expr Bool) -> L Bools term '[ls0, ls1] (Expr Bool)
+  BoolLit :: Bool -> L Bools term '[] (Expr Bool)
+
+data Ints
+data instance L Ints term ls tag where
+  Mult :: term ls0 (Expr Int) -> term ls1 (Expr Int) -> L Ints term '[ls0, ls1] (Expr Int)
+  IntLit :: Int -> L Ints term '[] (Expr Int)
+
+data IntBoolConversions
+data instance L IntBoolConversions term ls tag where
+  IntAsBool :: term ls0 (Expr Int) -> L IntBoolConversions term '[ls0] (Expr Bool)
+  BoolAsInt :: term ls0 (Expr Bool) -> L IntBoolConversions term '[ls0] (Expr Int)
+
+data SList (xs :: [a]) where
+  SConsNil :: SList '[x]
+  SCons :: SList xs -> SList (x ': xs)
+ 
+data BoolAST = BAXor BoolAST BoolAST | BAT | BAF
+
+send :: CRBFlat ('[l] ': lss) ls => L l Term lss tag -> Term ls tag
+send = Send rbflat
+sendInto :: CRBInto ('[l] ': lss) ls => L l Term lss tag -> Term ls tag
+sendInto = Send rbinto
+ 
+example_1 :: Term '[Bools, Bools, Bools] (Expr Bool)
+example_1 = send $ Xor (send $ BoolLit True) (send $ BoolLit False)
+
+--example_2 :: Term '[Bools, Bools, Bools] (Expr Bool)
+--example_2 = send $ Xor (send $ BoolLit True) (send $ BoolLit False)
+
+{-
+conv_1 :: Term '[Bools] (Expr Bool) -> Term '[Ints] (Expr Int)
+conv_1 (Send (RB2 ListEqMod1Base SWBase next) (BoolLit b)) = send $ IntLit $ if b then -1 else 1
+conv_1 (Send (RB2 (ListEqMod1Step idx) SWBase _) _) = case idx of
+conv_1 (Send (RB2 _ (SWInc (SWInc _)) next) _) = case next of
+conv_1 (Send (RB2 _ (SWInc SWBase) next) _) = case next of
+conv_1 (Send (RB2 ListEqMod1Base SWBase (RB1 (RB1 (RB1 next)))) x) = case next of
+
+conv_2 :: Term (Bools ': ls) (Expr Bool) -> Term (IntBoolConversions ': Ints ': ls) (Expr Bool)
+conv_2 (Send FlattenedBase (BoolLit b)) = send $ IntAsBool $ send $ IntLit $ if b then -1 else 1
+
+conv_3 :: Term (Bools ': ls) (Expr Bool) -> Term (IntBoolConversions ': Ints ': IntBoolConversions ': ls) (Expr Int)
+conv_3 (Send lengths (Xor (x :: Term ls0 (Expr Bool)) y)) = Bring (ListEqMod1Step $ ListEqMod1Step $ h lengths) $ Send (FlattenedStep $ f $ FlattenedStep $ FlattenedBase') $ Mult (Send FlattenedBase' $ BoolAsInt x) (Send FlattenedBase' $ BoolAsInt y) where
+  f :: Flattened xss xs -> Flattened (ls0 ': xss) (ListAppend ls0 xs)
+  f = g (j lengths)
+  g :: SList ys -> Flattened xss xs -> Flattened (ys ': xss) (ListAppend ys xs)
+  g SConsNil x = FlattenedSeg x
+  g (SCons s) x = FlattenedStep $ g s x
+  h :: Flattened '[xs, ys] zs -> ListEqMod1 zs (ListAppend xs (x ': ys)) x
+  h (FlattenedStep x) = ListEqMod1Step $ h x
+  h (FlattenedSeg x) = case i x of Refl -> ListEqMod1Step ListEqMod1Base
+  i :: Flattened '[xs] ys -> xs :~: ys
+  i FlattenedBase' = Refl
+  i (FlattenedSeg FlattenedBase) = Refl
+  i (FlattenedStep x) = case i x of Refl -> Refl
+  j :: Flattened '[xs, ys] zs -> SList xs
+  j (FlattenedSeg _) = SConsNil
+  j (FlattenedStep x) = SCons $ j x
+conv_3 (Bring idx x) = conv_4 idx x
+
+--conv_4 :: ListEqMod1 ls' ls Bools -> Term ls (Expr Bool) -> Term (IntBoolConversions ':) (Expr Int)
+--conv_4 (Bring idx (x :: Term _ (Expr Bool))) = _ $ conv_4 _ x
+---}
